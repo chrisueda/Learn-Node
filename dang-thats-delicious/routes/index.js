571822adeb6f029ae0ca438d15cce0410267442c
Christopher Ueda <chrisu@benefitcosmetics.com>
2018-12-18 18:27:11 -0800

    Create starter branch

---
+++ dang-thats-delicious/routes/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+// Do work here
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+
+router.get('/add', authController.isLoggedIn, storeController.addStore);
+
+router.post('/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.updateStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+
+router.get('/store/:slug', storeController.getStoreBySlug);
+
+// Tags
+router.get('/tags', catchErrors(storeController.getStoresByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+// Login
+router.get('/login', userController.loginForm);
+router.post('/login', authController.login)
+router.get('/register', userController.registerForm);
+
+// validate registration data
+// register user
+// log them in
+router.post('/register',
+  userController.validateRegister,
+  userController.register,
+  authController.login
+);
+
+router.get('/logout', authController.logout);
+
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
+
+module.exports = router;
